4c286596316bdfe4d621259a7bcc1819b76b1cf8
thefattestcat <48980192+thefattestcat@users.noreply.github.com>
2020-07-29 10:11:47 +1200

    initial server commit

---
+++ server/scraper.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer-core');
+const chromePath = require('../chromium.path.js')
+
+const myArgs = process.argv.slice(2);
+
+let url = 'https://easycrypto.ai/nz/rates';
+
+(async () => {
+    const browser = await puppeteer.launch({
+        executablePath: chromePath,
+        headless: myArgs[0] || false,
+    });
+
+    const page = await browser.newPage();
+    await page.goto(url);   
+    let pageData = await page.evaluate(() => {
+        let buttons = document.querySelectorAll('button');
+        return buttons;
+    })
+    console.log(pageData)
+    await browser.close();
+})();
+
