c8f0c47e167c05e86deddc3d6f05efc78de64cba
thefattestcat <48980192+thefattestcat@users.noreply.github.com>
2020-07-29 14:12:38 +1200

    Added Pair and Arbitrage classes

--- server/util/spread.class.js
+++ server/util/spread.class.js
@@ -1,12 +1,52 @@
+/**
+ * 
+ */
 class Pair {
-    constructor() {
+    constructor(symbol, bid, ask) {
+        this.symbol = symbol;
+        this.pair = this.splitSymbol(this.symbol);
+        this.bid = bid;
+        this.ask = ask;
+    }
 
+    splitSymbol(symbol) {
+        const base = symbol.substring(this.symbol.length - 3, this.symbol.length);
+        const quote = symbol.substring(0, symbol.length - 3);
+        return {base: base, quote: quote};
+    }
+
+    get base() {
+        return this.pair.base;
+    }
+
+    get quote() {
+        return this.pair.quote;
+    }
+
+    get bid() {
+        return this.bid;
+    }
+
+    get ask() {
+        return this.ask;
     }
 }
 
 class Arbitrage {
-    constructor() {
+    constructor(pair1, pair2, basePair) {
+        this.pair1 = pair1;
+        this.pair2 = pair2;
+        this.basePair = basePair;
+    }
 
+    calculateSpread() {
+        let spread = 0;
+        //Spread calc here
+        return spread;
+    }
+
+    get spread() {
+        return this.calculateSpread();
     }
 }
 
